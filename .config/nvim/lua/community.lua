89d75a70dda3491c9259af3d27c6ada2e0a7586d
jason scott <jsco2t@outlook.com>
2024-08-07 20:21:29 +0000

    Perf fixes for astronvim

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -8,10 +8,9 @@ return {
   { import = "astrocommunity.pack.lua" },
 
   -- statusline config
-  --{ import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  --{ import = "astrocommunity.recipes.heirline-mode-text-statusline" }, -- see statusline.lua - done manually
 
   -- tabline config
-  --{ import = "astrocommunity.bars-and-lines.bufferline-nvim" },
   { import = "astrocommunity.recipes.disable-tabline" },
 
   -- themes

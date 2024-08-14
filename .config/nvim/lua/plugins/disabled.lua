3c39a8bb54b2cdbba6b644092312db065aec7a2f
jason scott <jsco2t@outlook.com>
2024-08-14 22:45:39 +0000

    removing prior nvim config

--- .config/nvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
--- file is used to disable built-in / default plugins used by astronvim
-
-return {
-  --{ "rebelot/heirline.nvim", enabled = false }, -- customized statusline.lua has resolved most the perf issues
-  { "lukas-reineke/indent-blankline.nvim", enabled = false },
-}

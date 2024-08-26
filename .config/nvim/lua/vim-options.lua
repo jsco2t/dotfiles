9ab400a628090a3a639528a3086c3d45a73a0a19
jason scott <jsco2t@outlook.com>
2024-08-26 23:29:25 +0000

    testing clipboard functionality over ssh

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -16,6 +16,52 @@ vim.keymap.set("n", "<leader>ww", "gwip", { desc = "reflow text" })
 -- clear search highlights
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+-- [[ vim ui settings ]]
+--
+
+-- remove code folding
+vim.o.foldcolumn = "0"
+
+-- [[ vim os integration ]]
+--
+
+-- configure clipboard support
+--vim.opt.clipboard = "unnamedplus"
+
 -- perf testing
 -- vim.o.cursorline = false
-vim.o.foldcolumn = "0"
+--vim.opt.clipboard = "unnamedplus,unnamed"
+
+--if os.getenv "SSH_CLIENT" ~= nil or os.getenv "SSH_TTY" ~= nil then
+-- local function my_paste(_)
+--   return function(_)
+--     local content = vim.fn.getreg '"'
+--     return vim.split(content, "\n")
+--   end
+-- end
+--
+-- vim.g.clipboard = {
+--   name = "OSC 52",
+--   copy = {
+--     ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+--     ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+--   },
+--   paste = {
+--     ["+"] = my_paste "+",
+--     ["*"] = my_paste "*",
+--   },
+-- }
+--end
+--
+
+-- vim.g.clipboard = {
+--   name = "OSC 52",
+--   copy = {
+--     ["+"] = require("vim.ui.clipboard.osc52").copy "+",
+--     ["*"] = require("vim.ui.clipboard.osc52").copy "*",
+--   },
+--   paste = {
+--     ["+"] = require("vim.ui.clipboard.osc52").paste "+",
+--     ["*"] = require("vim.ui.clipboard.osc52").paste "*",
+--   },
+-- }

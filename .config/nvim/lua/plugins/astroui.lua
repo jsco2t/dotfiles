61b3b9b95e57874212ac2d4f08b078a5c69d33f4
jason scott <jsco2t@outlook.com>
2024-10-17 10:01:47 -0600

    adding support for changing status bar colors

--- .config/nvim/lua/plugins/astroui.lua
+++ .config/nvim/lua/plugins/astroui.lua
@@ -3,6 +3,11 @@ return {
   ---@type AstroUIOpts
   opts = {
     colorscheme = "nordic", -- 'kanagawa' is also good
+    highlights = {
+      init = {
+        HeirlineNormal = { bg = "#80B3B2" }, -- https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/plugins/_astroui_status.lua
+      },
+    },
   },
 }
 
--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -11,10 +11,12 @@ return {
     priority = 1000,
     config = function()
       require("nordic").setup {
+        -- theme matching colors can be found here: https://github.com/AlexvZyl/nordic.nvim/blob/main/lua/nordic/colors/nordic.lua
         on_palette = function(palette)
           palette.magenta.base = "#625eac"
           palette.magenta.bright = "#7b78b9"
           palette.magenta.dim = "#514d96"
+          palette.orange.bright = "#CB775D" --"#80B3B2" --"#fbd07b" --"#EBCB8B" -- used for `normal` statusline indicator
           return palette
         end,
       }

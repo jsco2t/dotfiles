900d2c53d163c45c005b5bbfa05bef88d34ae438
jason scott <jsco2t@outlook.com>
2024-10-18 12:42:07 -0600

    testing updated mason config

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,50 +1,44 @@
--- From the go community plugin we already get `gopls, gomodifytags, gotests, iferr, impl`
--- the following are additions which are helpful for go development.
-
--- return {
---   {
---     "williamboman/mason-lspconfig.nvim",
---     optional = true,
---     opts = function(_, opts)
---       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "golangci_lint_ls" })
---     end,
---   },
---   {
---     "WhoIsSethDaniel/mason-tool-installer.nvim",
---     optional = true,
---     opts = function(_, opts)
---       opts.ensure_installed = require("astrocore").list_insert_unique(
---         opts.ensure_installed,
---         { "staticcheck", "gofumpt", "goimports", "golangci_lint_ls" }
---       )
---     end,
---   },
--- }
---
-
 return {
-  -- {
-  --   "williamboman/mason-lspconfig.nvim",
-  --   optional = true,
-  --   opts = {
-  --     ensure_installed = {
-  --       "golangci_lint_ls",
-  --     },
-  --   },
-  -- },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "golangci_lint_ls",
+        "gopls",
+      })
+    end,
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "editorconfig-checker",
+        "golangci_lint",
+      })
+    end,
+  },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    --optional = true,
-    lazy = true,
-    opts = {
-      ensure_installed = {
+    optional = true,
+    opts = function(_, opts)
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+        "delve",
+        "gopls",
+        "gomodifytags",
+        "gotests",
+        "iferr",
+        "impl",
+        "goimports",
         "staticcheck",
         "gofumpt",
         "goimports",
         "goimports-reviser",
         "golangci_lint",
         "markdownlint",
-      },
-    },
+        "editorconfig-checker",
+      })
+    end,
   },
 }

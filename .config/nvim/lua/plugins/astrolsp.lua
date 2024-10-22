63aeb5d69eed8f9d4ecb517fe8de011345c61d0d
jason scott <jsco2t@outlook.com>
2024-10-22 17:18:34 -0600

    tuning golang linting

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -14,5 +14,56 @@ return {
         enabled = true,
       },
     },
+    -- based on: https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/pack/go/init.lua
+    -- included here to disable `ST1003`
+    ---@diagnostic disable-next-line: missing-fields
+    config = {
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              ST1003 = false,
+              fieldalignment = false,
+              fillreturns = true,
+              nilness = true,
+              nonewvars = true,
+              shadow = true,
+              undeclaredname = true,
+              unreachable = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            codelenses = {
+              gc_details = true, -- Show a code lens toggling the display of gc's choices.
+              generate = true, -- show the `go generate` lens.
+              regenerate_cgo = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+            buildFlags = { "-tags", "integration" },
+            completeUnimported = true,
+            diagnosticsDelay = "500ms",
+            gofumpt = true,
+            matcher = "Fuzzy",
+            semanticTokens = true,
+            staticcheck = true,
+            symbolMatcher = "fuzzy",
+            usePlaceholders = true,
+          },
+        },
+      },
+    },
   },
 }

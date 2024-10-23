e98678c56c97ea996e1949d277bfa28189f6faf8
jason scott <jsco2t@outlook.com>
2024-10-23 09:20:12 -0600

    removing prior linting tuning for golang

--- .config/nvim/lua/plugins/astrolsp.lua
+++ .config/nvim/lua/plugins/astrolsp.lua
@@ -17,53 +17,53 @@ return {
     -- based on: https://github.com/AstroNvim/astrocommunity/blob/main/lua/astrocommunity/pack/go/init.lua
     -- included here to disable `ST1003`
     ---@diagnostic disable-next-line: missing-fields
-    config = {
-      gopls = {
-        settings = {
-          gopls = {
-            analyses = {
-              ST1003 = true,
-              fieldalignment = false,
-              fillreturns = true,
-              nilness = true,
-              nonewvars = true,
-              shadow = true,
-              undeclaredname = true,
-              unreachable = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
-            },
-            codelenses = {
-              gc_details = true, -- Show a code lens toggling the display of gc's choices.
-              generate = true, -- show the `go generate` lens.
-              regenerate_cgo = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
-            },
-            hints = {
-              assignVariableTypes = true,
-              compositeLiteralFields = true,
-              compositeLiteralTypes = true,
-              constantValues = true,
-              functionTypeParameters = true,
-              parameterNames = true,
-              rangeVariableTypes = true,
-            },
-            buildFlags = { "-tags", "integration" },
-            completeUnimported = true,
-            diagnosticsDelay = "500ms",
-            gofumpt = true,
-            matcher = "Fuzzy",
-            semanticTokens = true,
-            staticcheck = true,
-            symbolMatcher = "fuzzy",
-            usePlaceholders = true,
-          },
-        },
-      },
-    },
+    -- config = {
+    --   gopls = {
+    --     settings = {
+    --       gopls = {
+    --         analyses = {
+    --           ST1003 = false, -- <-- <-- <--
+    --           fieldalignment = false,
+    --           fillreturns = true,
+    --           nilness = true,
+    --           nonewvars = true,
+    --           shadow = true,
+    --           undeclaredname = true,
+    --           unreachable = true,
+    --           unusedparams = true,
+    --           unusedwrite = true,
+    --           useany = true,
+    --         },
+    --         codelenses = {
+    --           gc_details = true, -- Show a code lens toggling the display of gc's choices.
+    --           generate = true, -- show the `go generate` lens.
+    --           regenerate_cgo = true,
+    --           test = true,
+    --           tidy = true,
+    --           upgrade_dependency = true,
+    --           vendor = true,
+    --         },
+    --         hints = {
+    --           assignVariableTypes = true,
+    --           compositeLiteralFields = true,
+    --           compositeLiteralTypes = true,
+    --           constantValues = true,
+    --           functionTypeParameters = true,
+    --           parameterNames = true,
+    --           rangeVariableTypes = true,
+    --         },
+    --         buildFlags = { "-tags", "integration" },
+    --         completeUnimported = true,
+    --         diagnosticsDelay = "500ms",
+    --         gofumpt = true,
+    --         matcher = "Fuzzy",
+    --         semanticTokens = true,
+    --         staticcheck = true,
+    --         symbolMatcher = "fuzzy",
+    --         usePlaceholders = true,
+    --       },
+    --     },
+    --   },
+    -- },
   },
 }

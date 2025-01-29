84c9fa1ff25f551e8e04a606593cecd592e72fca
jason scott <jsco2t@outlook.com>
2025-01-28 17:20:48 -0700

    moving back to kickstart based config

---
+++ .config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,26 @@
+return {
+  -- {
+  --   'navarasu/onedark.nvim',
+  --   priority = 1000, -- Ensure it loads first
+  --   init = function()
+  --     vim.cmd.colorscheme 'onedark'
+  --   end,
+  -- },
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    init = function()
+      require('onedark').setup {
+        style = 'darker',
+        colors = {
+          -- https://github.com/navarasu/onedark.nvim/blob/master/lua/onedark/palette.lua
+          purple = '#726fb5',
+          --purple = '#625eac', -- previous theme
+          --purple = '#7b78b9', -- lighter
+          --purple = '#514d96', -- darker
+        },
+      }
+      vim.cmd.colorscheme 'onedark'
+    end,
+  },
+}

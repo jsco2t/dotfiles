eb601981d37e5408188456cd27a26969353f9123
jason scott <jsco2t@outlook.com>
2025-08-26 14:45:34 +0000

    wip

---
+++ .config/nvim/lua/lazy-plugins.lua
@@ -0,0 +1,58 @@
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+require('lazy').setup({
+
+  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+
+  require 'kickstart.plugins.gitsigns',
+
+  require 'kickstart.plugins.which-key',
+
+  require 'kickstart.plugins.telescope',
+
+  require 'kickstart.plugins.lsp',
+
+  require 'kickstart.plugins.autoformat',
+
+  require 'kickstart.plugins.autopairs',
+
+  require 'kickstart.plugins.autocompletion',
+
+  require 'kickstart.plugins.theme',
+
+  require 'kickstart.plugins.comments',
+
+  require 'kickstart.plugins.mini',
+
+  require 'kickstart.plugins.treesitter',
+
+  { import = 'custom.plugins' },
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})

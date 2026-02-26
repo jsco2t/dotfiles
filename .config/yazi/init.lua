f8ea66834af07ea8ff2529e5fde7e5695baf8ab5
jason scott <jsco2t@outlook.com>
2026-02-26 09:30:51 -0700

    tuning yazi config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,5 +1,17 @@
 -- https://github.com/yazi-rs/plugins/tree/main/git.yazi
 
+-- style info for git markers
+th.git = th.git or {}
+th.git.modified = ui.Style():fg("blue")
+th.git.deleted = ui.Style():fg("red"):bold()
+th.git.unknown_sign = " "
+th.git.modified_sign = "M"
+th.git.deleted_sign = "D"
+th.git.clean_sign = "✔"
+th.git.added_sign = "+"
+th.git.untracked_sign = "U"
+th.git.updated_sign = "!"
+
 require("git"):setup {
 	-- Order of status signs showing in the linemode
 	order = 1500,
